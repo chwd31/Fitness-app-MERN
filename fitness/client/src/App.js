eac5472db556739e548ed79259c21ebcfb98c2e1
chwd31 <chwdwile@aol.com>
2023-05-15 18:51:03 -0500

    React components App.js and index.js

--- fitness/client/src/App.js
+++ fitness/client/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";  
+import HomePage from "./components/HomePage";
+import ExercisePage from "./components/ExercisePage";
+import ProfilePage from "./components/ProfilePage";
+import WeeklyStatsPage from "./components/WeeklyStatsPage";
+import LoginPage from "./components/LoginPage";
+import SignupPage from "./components/SignupPage";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  return
+  <Router>
+    <NavBar />
+    <Switch>
+      <Route exact path="/" component={HomePage} />
+      <Route exact path="/exercise" component={ExercisePage} />
+      <Route exact path="/profile" component={ProfilePage} />
+      <Route exact path="/weeklystats" component={WeeklyStatsPage} />
+      <Route exact path="/login" component={LoginPage} />
+      <Route exact path="/signup" component={SignupPage} />
+    </Switch>
+    <Footer />
+  </Router>
+};
 
 export default App;
