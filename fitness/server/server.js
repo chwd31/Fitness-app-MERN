a2b33904b1d8d776c2c5602dc43b57dcd650132e
chwd31 <chwdwile@aol.com>
2023-05-17 11:48:43 -0500

    fixed restful apis by changing to graphql setup

--- fitness/server/server.js
+++ fitness/server/server.js
@@ -4,6 +4,12 @@ const path = require("path");
 const db = require("./config/connection");
 const { typeDefs, resolvers } = require("./schemas");
 const { authMiddleware } = require("./utils/auth");
+const userRoutes = require("./routes/users");
+const profileRoutes = require("./routes/profile");
+const exerciseRoutes = require("./routes/exercise");
+const weeklyStatsRoutes = require("./routes/weeklyStats");
+
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
@@ -11,16 +17,27 @@ const server = new ApolloServer({
     resolvers,
     context: authMiddleware,
 });
+
 server.applyMiddleware({ app });
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+app.use("/api/users", userRoutes);
+app.use("/api/profile", profileRoutes);
+app.use("/api/exercise", exerciseRoutes);
+app.use("/api/weekly-stats", weeklyStatsRoutes);
+
+
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../client/build")));
 }
+
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../client/build/index.html"));
 }
 );
+
 const startApolloServer = async () => {
     await server.start();
     await server.listen(PORT);
