1634cd54adad2f6bd3cffac25f11b68dd7605b71
chwd31 <chwdwile@aol.com>
2023-05-20 15:15:13 -0500

    home, login and signup displayed

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
 import './index.css';
+import './App.css';
 import App from './App';
 
 const stripePromise = loadStripe('pk_test_51N9GI3HfMu1TGSRk8pzPyHlcjvLSKxBPFBCabnywcWvobZQaU2FAzw2dub59w55XDbSeEA5oA143x1PVaHXnxmpq00xTYUKWlb');
 
+const client = new ApolloClient({
+  uri: 'http://localhost:3001/graphql',
+  cache: new InMemoryCache(),
+});
+
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Elements stripe={stripePromise}>
-      <App />
-    </Elements>
+    <ApolloProvider client={client}>
+      <Elements stripe={stripePromise}>
+        <App />
+      </Elements>
+    </ApolloProvider>
   </React.StrictMode>
 );
