e0e04485e7388f7f870bde3c3cdc9bba86c80574
chwd31 <chwdwile@aol.com>
2023-05-24 14:56:58 -0500

    deployment setup

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,18 +1,22 @@
+require('dotenv').config();
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const { AuthenticationError } = require('apollo-server-express');
 const { User } = require('../models');
 
+// Get the secret key from the environment variable
+const secretKey = process.env.JWT_SECRET_KEY;
+
 // Generates a JWT token
 const generateToken = (userId) => {
-  const token = jwt.sign({ userId }, 'your-secret-key', { expiresIn: '1h' });
+  const token = jwt.sign({ userId }, secretKey, { expiresIn: '1h' });
   return token;
 };
 
 // Verifies a JWT token and extracts the user ID
 const verifyToken = (token) => {
   try {
-    const decoded = jwt.verify(token, 'your-secret-key');
+    const decoded = jwt.verify(token, secretKey);
     return decoded.userId;
   } catch (error) {
     throw new AuthenticationError('Invalid or expired token');
